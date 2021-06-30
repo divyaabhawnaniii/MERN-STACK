688aecd5f0d49bb3a7e128fec5b02eb968897b9b
Divya Bhawnani <84888330+divyaabhawnaniii@users.noreply.github.com>
2021-06-30 14:52:51 +0530

    done

---
+++ TEST/test-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Footer } from "./Components/Footer";
+import { Navbar } from "./Components/Navbar";
+import Cardshow from "./Components/CardShow";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <CardShow/>
+      <Footer/>
+      </>
+  );
+}
+
+export default App
---
+++ TEST/test-app/src/Components/CardShow.js
@@ -0,0 +1,15 @@
+ import React from 'react'
+
+export const CardShow = () => {
+    return (
+        <div>
+            <Card
+            key = {post.id}
+            name = {post.name}
+            body = {post.body}
+        />
+        </div>
+    )
+}
+
+export default CardShow;
